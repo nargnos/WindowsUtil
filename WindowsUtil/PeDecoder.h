b433f2c05878b8310b94043b5a6c8f106c02bd4d
{S} <nargno.s@gmail.com>
2015-06-17 01:33:15 +0800

    读节
    读数据目录需要

--- WindowsUtil/PeDecoder.h
+++ WindowsUtil/PeDecoder.h
@@ -1,9 +1,7 @@
 #pragma once
-#include <iostream>
 #include <Windows.h>
 #include <WinBase.h>
 namespace PE{
-	using namespace std;
 	class PeDecoder
 	{
 	public:
@@ -14,48 +12,127 @@ namespace PE{
 		{
 			CheckImage();
 		}
-		bool Is32Image()
+
+		bool HasNtHeader32()
 		{
-			return (*imageType != IMAGE_NT_OPTIONAL_HDR64_MAGIC);
+			return (*imageType == IMAGE_NT_OPTIONAL_HDR32_MAGIC);
 		}
 		bool IsPE()
 		{
 			return isPE;
 		}
 
-
 		PIMAGE_DOS_HEADER DosHeader()
 		{
-			return reinterpret_cast<PIMAGE_DOS_HEADER>(base);
+			return (PIMAGE_DOS_HEADER)base;
 		}
 
 		PIMAGE_NT_HEADERS32 NtHeader32()
 		{
-			if (!Is32Image())
+			if (*imageType != IMAGE_NT_OPTIONAL_HDR32_MAGIC)
 			{
 				return NULL;
 			}
-			return reinterpret_cast<PIMAGE_NT_HEADERS32>(NtHeader());
+			return (PIMAGE_NT_HEADERS32)NtHeader();
 		}
 		PIMAGE_NT_HEADERS64 NtHeader64()
 		{
-			if (Is32Image())
+			if (*imageType != IMAGE_NT_OPTIONAL_HDR64_MAGIC)
 			{
 				return NULL;
 			}
-			return reinterpret_cast<PIMAGE_NT_HEADERS64>(NtHeader());
+			return (PIMAGE_NT_HEADERS64)NtHeader();
+		}
+		PIMAGE_ROM_HEADERS RomHeader()
+		{
+			if (*imageType == IMAGE_ROM_OPTIONAL_HDR_MAGIC)
+			{
+				return (PIMAGE_ROM_HEADERS)NtHeader();
+			}
+			return NULL;
+		}
+		PIMAGE_SECTION_HEADER RvaToSection(DWORD rva)
+		{
+
+			auto section = firstSectionHeader;
+			while (section < lastSectionHeader)
+			{
+				if (rva < section->VirtualAddress + AlignUp((UINT)(section->Misc.VirtualSize), (UINT)(*sectionAlignment)))
+				{
+					if (rva < (section->VirtualAddress))
+					{
+						return NULL;
+					}
+					else
+					{
+						return section;
+					}
+				}
+
+				section++;
+			}
+
+			return NULL;
+		}
+		PIMAGE_SECTION_HEADER OffsetToSection(DWORD fileOffset)
+		{
+			auto section = firstSectionHeader;
+
+			while (section < lastSectionHeader)
+			{
+				if (fileOffset < section->PointerToRawData + section->SizeOfRawData)
+				{
+					if (fileOffset < section->PointerToRawData)
+					{
+						return NULL;
+					}
+					else
+					{
+						return section;
+					}
+				}
+				section++;
+			}
+
+			return NULL;
+		}
+		DWORD RvaToOffset(DWORD rva)
+		{
+			if (rva > 0)
+			{
+				IMAGE_SECTION_HEADER *section = RvaToSection(rva);
+				if (section == NULL)
+				{
+					return rva;
+				}
+
+				return rva - (section->VirtualAddress) + (section->PointerToRawData);
+			}
+			return 0;
+		}
+		
+		DWORD OffsetToRva(DWORD fileOffset)
+		{
+			if (fileOffset > 0)
+			{
+				IMAGE_SECTION_HEADER *section = OffsetToSection(fileOffset);
+				if (section != NULL)
+				{
+					return fileOffset - (section->PointerToRawData) + (section->VirtualAddress);
+				}
+			}
+			return 0;
 		}
 		PIMAGE_EXPORT_DIRECTORY ImageDirectoryEntryExport()
 		{
-			auto dir = &imageDataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
-			if (DataDirectoryExist(dir))
+			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_EXPORT);
+			if (!dir)
 			{
-				return dir->VirtualAddress
-				//ptr
-				//result->
+				return NULL;
 			}
+			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}
-		void ImageDirectoryEntryImport()
+		/*void ImageDirectoryEntryImport()
 		{
 			auto result = &imageDataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
 			if (DataDirectoryExist(result))
@@ -152,7 +229,7 @@ namespace PE{
 			if (DataDirectoryExist(result))
 			{
 			}
-		}
+		}*/
 		~PeDecoder()
 		{
 		}
@@ -160,11 +237,23 @@ namespace PE{
 		PCHAR base;
 		PVOID ntHeader = NULL;
 		PIMAGE_DATA_DIRECTORY imageDataDirectory = NULL;
+		PDWORD imageDataDirectorySize = 0;
+		PIMAGE_SECTION_HEADER firstSectionHeader = NULL;
+		PIMAGE_SECTION_HEADER lastSectionHeader = NULL;
+		PDWORD sectionAlignment = 0;
+		PWORD sectionCount = 0;
 		bool isMapped;
 		bool isPE;
 		PWORD imageType; //IMAGE_NT_OPTIONAL_HDR32_MAGIC IMAGE_NT_OPTIONAL_HDR64_MAGIC IMAGE_ROM_OPTIONAL_HDR_MAGIC
+
+		UINT AlignUp(UINT value, UINT alignment)
+		{
+			return (value + alignment - 1)&~(alignment - 1);
+		}
+
 		void CheckImage()
 		{
+			//  32 64位共用一些字段, 所以可以用NtHeader32
 			if (DosHeader()->e_magic != IMAGE_DOS_SIGNATURE || NtHeader32()->Signature != IMAGE_NT_SIGNATURE)
 			{
 				isPE = false;
@@ -173,24 +262,54 @@ namespace PE{
 			{
 				isPE = true;
 			}
+			// 设置下面这个值后就禁止共用
 			imageType = &NtHeader32()->OptionalHeader.Magic;
+			
 			switch (*imageType)
 			{
 			case IMAGE_NT_OPTIONAL_HDR32_MAGIC:
-				imageDataDirectory = NtHeader32()->OptionalHeader.DataDirectory;
+				auto header32 = NtHeader32();
+				imageDataDirectory = header32->OptionalHeader.DataDirectory;
+				imageDataDirectorySize = &header32->OptionalHeader.NumberOfRvaAndSizes;
+				sectionAlignment = &header32->OptionalHeader.SectionAlignment;
+				firstSectionHeader = IMAGE_FIRST_SECTION(header32);
+				sectionCount = &header32->FileHeader.NumberOfSections;	
+				
 				break;
 			case IMAGE_NT_OPTIONAL_HDR64_MAGIC:
-				imageDataDirectory = NtHeader64()->OptionalHeader.DataDirectory;
+				auto header64 = NtHeader64();
+				imageDataDirectory = header64->OptionalHeader.DataDirectory;
+				imageDataDirectorySize = &header64->OptionalHeader.NumberOfRvaAndSizes;
+				sectionAlignment = &header64->OptionalHeader.SectionAlignment;
+				firstSectionHeader = IMAGE_FIRST_SECTION(header64);
+				sectionCount = &header64->FileHeader.NumberOfSections;
+				break;
+				
+			case IMAGE_ROM_OPTIONAL_HDR_MAGIC:
+				// 第三种没见过, 定义里没节和数据目录, 不做解析
 				break;
 			default:
-				// 第三种没见过
+				
 				break;
 			}
+			lastSectionHeader = firstSectionHeader + *sectionCount;
+
+
 		}
-		bool DataDirectoryExist(PIMAGE_DATA_DIRECTORY dir)
-		{
-			return dir->VirtualAddress != NULL;
+		PIMAGE_DATA_DIRECTORY GetDataDirectory(DWORD index)
+		{			
+			if (*imageDataDirectorySize > index && IMAGE_NUMBEROF_DIRECTORY_ENTRIES > index)
+			{
+				auto result = &imageDataDirectory[index];
+				if (result->VirtualAddress != NULL)
+				{
+					return result;
+				}
+			}			
+			return NULL;
 		}
+
+		
 		PVOID NtHeader()
 		{
 			if (ntHeader == NULL)
@@ -199,7 +318,19 @@ namespace PE{
 			}
 			return ntHeader;
 		}
-
+		PVOID GetRvaData(DWORD rva)
+		{
+			DWORD offset;
+			if (isMapped)
+			{
+				offset = rva;
+			}
+			else
+			{
+				offset = RvaToOffset(rva);
+			}
+			return base + offset;
+		}
 	};
 	class ExportDirectoryReader
 	{
