c78f873c40218a4e3acf15a9aa85114259c83bbf
[S] <nargno.s@gmail.com>
2015-06-23 00:59:45 +0800

    添加了一些测试代码
    测试了导出表之前的函数, 导入表读取代码编写补完但未测试, 改了一些bug不记得改了什么了

--- WindowsUtil/PeDecoder.h
+++ WindowsUtil/PeDecoder.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <Windows.h>
 #include <WinBase.h>
-namespace PE{
+namespace PE
+{
+	// PE解析器
 	class PeDecoder
 	{
 	public:
@@ -21,7 +23,10 @@ namespace PE{
 		{
 			return isPE;
 		}
-
+		PVOID GetBase()
+		{
+			return base;
+		}
 		PIMAGE_DOS_HEADER DosHeader()
 		{
 			return (PIMAGE_DOS_HEADER)base;
@@ -53,7 +58,6 @@ namespace PE{
 		}
 		PIMAGE_SECTION_HEADER RvaToSection(DWORD rva)
 		{
-
 			auto section = firstSectionHeader;
 			while (section < lastSectionHeader)
 			{
@@ -110,7 +114,7 @@ namespace PE{
 			}
 			return 0;
 		}
-		
+
 		DWORD OffsetToRva(DWORD fileOffset)
 		{
 			if (fileOffset > 0)
@@ -124,7 +128,7 @@ namespace PE{
 			return 0;
 		}
 
-		PIMAGE_EXPORT_DIRECTORY ImageExport()
+		PIMAGE_EXPORT_DIRECTORY GetImageExport()
 		{
 			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_EXPORT);
 			if (!dir)
@@ -134,7 +138,7 @@ namespace PE{
 			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}
 
-		PIMAGE_IMPORT_DESCRIPTOR ImageImport()
+		PIMAGE_IMPORT_DESCRIPTOR GetImageImport()
 		{
 			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_IMPORT);
 			if (!dir)
@@ -143,7 +147,7 @@ namespace PE{
 			}
 			return (PIMAGE_IMPORT_DESCRIPTOR)GetRvaData(dir->VirtualAddress);
 		}
-		PIMAGE_RESOURCE_DIRECTORY ImageResource()
+		PIMAGE_RESOURCE_DIRECTORY GetImageResource()
 		{
 			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_RESOURCE);
 			if (!dir)
@@ -154,23 +158,23 @@ namespace PE{
 		}
 		/*void ImageDirectoryEntryException()
 		{
-			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_EXCEPTION);
-			if (!dir)
-			{
-				return NULL;
-			}
-			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
+		auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_EXCEPTION);
+		if (!dir)
+		{
+		return NULL;
+		}
+		return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}*/
 		/*void ImageDirectoryEntrySecurity()
 		{
-			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_SECURITY);
-			if (!dir)
-			{
-				return NULL;
-			}
-			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
+		auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_SECURITY);
+		if (!dir)
+		{
+		return NULL;
+		}
+		return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}*/
-		PIMAGE_RELOCATION ImageBasereloc()
+		PIMAGE_RELOCATION GetImageBasereloc()
 		{
 			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_BASERELOC);
 			if (!dir)
@@ -181,40 +185,40 @@ namespace PE{
 		}
 		/*void ImageDirectoryEntryDebug()
 		{
-			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_DEBUG);
-			if (!dir)
-			{
-				return NULL;
-			}
-			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
+		auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_DEBUG);
+		if (!dir)
+		{
+		return NULL;
+		}
+		return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}
 		void ImageDirectoryEntryArchitecture()
 		{
-			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_ARCHITECTURE);
-			if (!dir)
-			{
-				return NULL;
-			}
-			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
+		auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_ARCHITECTURE);
+		if (!dir)
+		{
+		return NULL;
+		}
+		return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}
 		void ImageDirectoryEntryGlobalptr()
 		{
-			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_GLOBALPTR);
-			if (!dir)
-			{
-				return NULL;
-			}
-			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
+		auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_GLOBALPTR);
+		if (!dir)
+		{
+		return NULL;
+		}
+		return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}*/
 
 		/*void ImageDirectoryEntryTls() // 这个有32 64区分
 		{
-			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_TLS);
-			if (!dir)
-			{
-				return NULL;
-			}
-			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
+		auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_TLS);
+		if (!dir)
+		{
+		return NULL;
+		}
+		return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}*/
 		//void ImageDirectoryEntryLoadConfig() //PIMAGE_LOAD_CONFIG_DIRECTORY64
 		//{
@@ -227,40 +231,40 @@ namespace PE{
 		//}
 		/*void ImageDirectoryEntryBoundImport()
 		{
-			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT);
-			if (!dir)
-			{
-				return NULL;
-			}
-			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
+		auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT);
+		if (!dir)
+		{
+		return NULL;
+		}
+		return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}*/
 		/*DWORD ImageDirectoryEntryIat()
 		{
-			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_IAT);
-			if (!dir)
-			{
-				return NULL;
-			}
-			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
+		auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_IAT);
+		if (!dir)
+		{
+		return NULL;
+		}
+		return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}*/
 		/*
 		void ImageDirectoryEntryDelayImport()
 		{
-			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT);
-			if (!dir)
-			{
-				return NULL;
-			}
-			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
+		auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT);
+		if (!dir)
+		{
+		return NULL;
+		}
+		return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}
 		void ImageDirectoryEntryComDescriptor()
 		{
-			auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR);
-			if (!dir)
-			{
-				return NULL;
-			}
-			return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
+		auto dir = GetDataDirectory(IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR);
+		if (!dir)
+		{
+		return NULL;
+		}
+		return (PIMAGE_EXPORT_DIRECTORY)GetRvaData(dir->VirtualAddress);
 		}*/
 		~PeDecoder()
 		{
@@ -289,6 +293,14 @@ namespace PE{
 				}
 			}
 			return NULL;
+		}
+		PIMAGE_SECTION_HEADER GetFirstSectionHeader()
+		{
+			return firstSectionHeader;
+		}
+		PWORD GetNumberOfSection()
+		{
+			return sectionCount;
 		}
 	private:
 		PCHAR base;
@@ -320,45 +332,49 @@ namespace PE{
 			lastSectionHeader = NULL;
 			sectionAlignment = NULL;
 			sectionCount = NULL;
-			
+
 
 			//  32 64位共用一些字段, 所以可以用NtHeader32
-			if (DosHeader()->e_magic != IMAGE_DOS_SIGNATURE || NtHeader32()->Signature != IMAGE_NT_SIGNATURE)
+			if (DosHeader()->e_magic != IMAGE_DOS_SIGNATURE || ((PIMAGE_NT_HEADERS32)NtHeader())->Signature != IMAGE_NT_SIGNATURE)
 			{
 				isPE = false;
+				return;
 			}
 			else
 			{
 				isPE = true;
 			}
 			// 设置下面这个值后就禁止共用
-			imageType = &NtHeader32()->OptionalHeader.Magic;
-			
+			imageType = &((PIMAGE_NT_HEADERS32)NtHeader())->OptionalHeader.Magic;
+
 			switch (*imageType)
 			{
 			case IMAGE_NT_OPTIONAL_HDR32_MAGIC:
+			{
 				auto header32 = NtHeader32();
 				imageDataDirectoryEntry = header32->OptionalHeader.DataDirectory;
 				imageDataDirectorySize = &header32->OptionalHeader.NumberOfRvaAndSizes;
 				sectionAlignment = &header32->OptionalHeader.SectionAlignment;
 				firstSectionHeader = IMAGE_FIRST_SECTION(header32);
-				sectionCount = &header32->FileHeader.NumberOfSections;	
-				
-				break;
+				sectionCount = &header32->FileHeader.NumberOfSections;
+			}
+			break;
 			case IMAGE_NT_OPTIONAL_HDR64_MAGIC:
+			{
 				auto header64 = NtHeader64();
 				imageDataDirectoryEntry = header64->OptionalHeader.DataDirectory;
 				imageDataDirectorySize = &header64->OptionalHeader.NumberOfRvaAndSizes;
 				sectionAlignment = &header64->OptionalHeader.SectionAlignment;
 				firstSectionHeader = IMAGE_FIRST_SECTION(header64);
 				sectionCount = &header64->FileHeader.NumberOfSections;
-				break;
-				
+			}
+			break;
+
 			case IMAGE_ROM_OPTIONAL_HDR_MAGIC:
 				// 第三种没见过, 定义里没节和数据目录, 不做解析
 				break;
 			default:
-				
+
 				break;
 			}
 			lastSectionHeader = firstSectionHeader + *sectionCount;
@@ -367,7 +383,7 @@ namespace PE{
 		}
 
 
-		
+
 		PVOID NtHeader()
 		{
 			if (ntHeader == NULL)
@@ -376,24 +392,19 @@ namespace PE{
 			}
 			return ntHeader;
 		}
-		
-	};
-	// 导入表读取器
-	class ImportThunkReader
-	{
-	public:
-		ImportThunkReader(){}
-		~ImportThunkReader(){}
-
-	private:
 
 	};
+
+
+
+	// 导入表Descriptor结构读取器
 	class ImportDescriptorReader
 	{
 	public:
 		ImportDescriptorReader(PeDecoder& pe)
 		{
-			descriptor = pe.ImageImport();
+
+			descriptor = pe.GetImageImport();
 			if (descriptor)
 			{
 				descriptorLength = &pe.GetDataDirectory(IMAGE_DIRECTORY_ENTRY_IMPORT)->Size;
@@ -402,7 +413,7 @@ namespace PE{
 			{
 				descriptorLength = 0;
 			}
-
+			Reset();
 		}
 		~ImportDescriptorReader(){}
 		bool Next()
@@ -411,6 +422,11 @@ namespace PE{
 			{
 				if (++currentIndex < *descriptorLength)
 				{
+					auto checkResult = descriptor[currentIndex];
+					if (checkResult.Name == NULL || checkResult.FirstThunk == NULL)
+					{
+						return false;
+					}
 					return true;
 				}
 			}
@@ -434,11 +450,128 @@ namespace PE{
 		DWORD currentIndex;
 		PIMAGE_IMPORT_DESCRIPTOR descriptor;
 	};
-	// 导出表读取器
+
+	// 导入表Thunk结构读取器
+#define CONVERT_THUNK_POINTER(thunkPointer, x) ((PIMAGE_THUNK_DATA##x)thunkPointer)
+	class ImportThunkReader
+	{
+	public:
+		ImportThunkReader(PeDecoder& pe, PIMAGE_IMPORT_DESCRIPTOR importDescriptor) :
+			importDescriptor(importDescriptor),
+			pe(pe)
+		{
+			is32 = pe.HasNtHeader32();
+			originalThunk = pe.GetRvaData(importDescriptor->OriginalFirstThunk);
+			thunk = pe.GetRvaData(importDescriptor->FirstThunk);
+			Reset();
+		}
+
+		PIMAGE_THUNK_DATA32 CurrentThunk32()
+		{
+			if (is32)
+			{
+				return CONVERT_THUNK_POINTER(thunk, 32);
+			}
+			return NULL;
+		}
+		PIMAGE_THUNK_DATA32 CurrentOriginalThunk32()
+		{
+			if (is32)
+			{
+				return CONVERT_THUNK_POINTER(originalThunk, 32);
+			}
+			return NULL;
+		}
+		PIMAGE_THUNK_DATA64 CurrentThunk64()
+		{
+			if (is32)
+			{
+				return NULL;
+			}
+			return CONVERT_THUNK_POINTER(thunk, 64);
+		}
+
+		PIMAGE_THUNK_DATA64 CurrentOriginalThunk64()
+		{
+			if (is32)
+			{
+				return NULL;
+			}
+			return CONVERT_THUNK_POINTER(originalThunk, 64);
+		}
+
+		bool IsSnapByOrdinal(PIMAGE_THUNK_DATA32 thunk)
+		{			
+			return IMAGE_SNAP_BY_ORDINAL32(thunk->u1.Ordinal);
+		}
+		bool IsSnapByOrdinal(PIMAGE_THUNK_DATA64 thunk)
+		{
+			return IMAGE_SNAP_BY_ORDINAL64(thunk->u1.Ordinal);
+		}
+		ULONGLONG GetOrdinal(PIMAGE_THUNK_DATA64 thunk)
+		{
+			return IMAGE_ORDINAL64(thunk->u1.Ordinal);
+		}
+		DWORD GetOrdinal(PIMAGE_THUNK_DATA32 thunk)
+		{
+			return IMAGE_ORDINAL32(thunk->u1.Ordinal);
+		}
+		PIMAGE_IMPORT_BY_NAME GetImportByNameStruct(PIMAGE_THUNK_DATA32 thunk)
+		{
+			return (PIMAGE_IMPORT_BY_NAME)pe.GetRvaData(thunk->u1.AddressOfData);
+		}
+		PIMAGE_IMPORT_BY_NAME GetImportByNameStruct(PIMAGE_THUNK_DATA64 thunk)
+		{
+			return (PIMAGE_IMPORT_BY_NAME)pe.GetRvaData(thunk->u1.AddressOfData);
+		}
+		bool Next()
+		{
+#define CHECK_THUNK(x) auto tmpThunk = CONVERT_THUNK_POINTER(currentThunk, x) + 1;\
+			if (CONVERT_THUNK_POINTER(currentThunk,x)->u1.AddressOfData == NULL)\
+			{\
+				return false;\
+			}\
+			currentThunk = tmpThunk;\
+			currentOriginalThunk = CONVERT_THUNK_POINTER(currentOriginalThunk, x) + 1;
+
+
+			if (is32)
+			{
+				CHECK_THUNK(32);
+			}
+			else
+			{
+				CHECK_THUNK(64);
+			}
+
+
+			return true;
+		}
+		void Reset()
+		{
+			currentThunk = thunk;
+			currentOriginalThunk = originalThunk;
+		}
+		~ImportThunkReader(){}
+
+	private:
+		PIMAGE_IMPORT_DESCRIPTOR importDescriptor;
+		bool is32;
+		PVOID currentThunk;
+		PVOID currentOriginalThunk;
+		PeDecoder& pe;
+		PVOID originalThunk;
+		PVOID thunk;
+
+	};
+
+
+
+	// 导出表读取器 TODO: 添加一个c++函数名称翻译器,或找一下系统里面有没有现成函数
 	class ExportReader
 	{
 	public:
-		ExportReader(PeDecoder& pe) :ExportReader(pe, pe.ImageExport())
+		ExportReader(PeDecoder& pe) :ExportReader(pe, pe.GetImageExport())
 		{
 		}
 		ExportReader(PeDecoder& pe, PIMAGE_EXPORT_DIRECTORY exportDirectory)
@@ -454,9 +587,8 @@ namespace PE{
 			else
 			{
 				numberOfFunctions = NULL;
-				currentIndex = -1;
 			}
-
+			Reset();
 		}
 		~ExportReader(){}
 		PIMAGE_EXPORT_DIRECTORY GetExportDirectory()
@@ -478,6 +610,14 @@ namespace PE{
 				return NULL;
 			}
 			return &names[nameOrdinals[currentIndex]];
+		}
+		PWORD CurrentNameOrdinals()
+		{
+			if (currentIndex == -1)
+			{
+				return NULL;
+			}
+			return &nameOrdinals[currentIndex];
 		}
 		bool Next()
 		{
@@ -503,15 +643,23 @@ namespace PE{
 		DWORD currentIndex;
 	};
 
-	// 节读取器, 使用比较频繁, 就不使用下标形式寻址了
+	// 节读取器
 	class SectionReader
 	{
 	public:
-		SectionReader(PIMAGE_SECTION_HEADER firstSectionHeader, WORD sectionCount)
+		
+		SectionReader(PIMAGE_SECTION_HEADER firstSectionHeader, PIMAGE_SECTION_HEADER lastSectionHeader):
+			firstSectionHeader(firstSectionHeader),
+			lastSectionHeader(lastSectionHeader)
 		{
-			this->firstSectionHeader = firstSectionHeader;
-			lastSectionHeader = firstSectionHeader + sectionCount;
 			Reset();
+		}
+		SectionReader(PIMAGE_SECTION_HEADER firstSectionHeader, WORD sectionCount):
+			SectionReader(firstSectionHeader, firstSectionHeader + sectionCount)
+		{
+		}
+		SectionReader(PeDecoder& pe) :SectionReader(pe.GetFirstSectionHeader(), *pe.GetNumberOfSection())
+		{
 		}
 		~SectionReader(){}
 		PIMAGE_SECTION_HEADER Current()
@@ -522,14 +670,15 @@ namespace PE{
 		{
 			if (sectionPointer)
 			{
-				if (sectionPointer != lastSectionHeader)
+				auto tmpSectionPointer = sectionPointer + 1;
+				if (tmpSectionPointer != lastSectionHeader)
 				{
-					sectionPointer++;
+					sectionPointer = tmpSectionPointer;
 				}
 				else
 				{
 					return false;
-				}				
+				}
 			}
 			else
 			{
@@ -546,5 +695,6 @@ namespace PE{
 		PIMAGE_SECTION_HEADER lastSectionHeader;
 		PIMAGE_SECTION_HEADER sectionPointer;
 	};
+
 
 }
--- test/test.cpp
+++ test/test.cpp
@@ -2,16 +2,309 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <iomanip>
 //#include "../LazyLoad/LazyLoad.h"
 #include "../WindowsUtil/EnvironmentBlock.h"
+#include "../WindowsUtil/PeDecoder.h"
+using namespace std;
+
+
+void TestPeDecoder()
+{
+	const int maxSpace = 50;
+	const int minSpace = 2;
+	int space = minSpace;
+	
+#define TO_RIGHT space+=2;if(space>maxSpace){space=maxSpace;}{
+#define TO_LEFT }space-=2;if(space<minSpace){space=minSpace;}
+#define DISPLAY_STRUCT_HEX(structVal, name) cout <<setw(space)<<""<<setw(maxSpace-space)<< #name <<" - 0x"<< hex<< structVal->name << endl
+#define DISPLAY_STRUCT_HEX2(structVal, name) cout <<setw(space)<<""<<setw(maxSpace-space)<< #name <<" - 0x"<< hex<< structVal.name << endl
+#define DISPLAY_STRUCT_POINTER_HEX(structVal, name) cout<<setw(space)<<""<<setw(maxSpace-space)<< #name <<" - 0x"<< hex<< &structVal->name << endl
+
+#define DISPLAY_HEX(name,val) cout <<setw(space)<<""<<setw(maxSpace-space)<<name <<" - 0x"<< hex << val<< endl
+#define DISPLAY_MSG(msg) cout <<setw(space)<<""<<msg<< endl
+
+	cout.setf(ios::left | ios::boolalpha);
+
+	// pe解析器测试, 只做测试怎么简单怎么编
+	PE::PeDecoder pe(GetModuleHandleA(NULL), true);
+	if (pe.IsPE())
+	{
+		// dosHeader
+		auto dosHeader = pe.DosHeader();		
+		DISPLAY_HEX("[Dos Header]", dosHeader);		
+		TO_RIGHT
+			DISPLAY_STRUCT_HEX(dosHeader, e_magic);
+			DISPLAY_STRUCT_HEX(dosHeader, e_cblp);
+			DISPLAY_STRUCT_HEX(dosHeader, e_cp);
+			DISPLAY_STRUCT_HEX(dosHeader, e_crlc);
+			DISPLAY_STRUCT_HEX(dosHeader, e_cparhdr);
+			DISPLAY_STRUCT_HEX(dosHeader, e_minalloc);
+			DISPLAY_STRUCT_HEX(dosHeader, e_maxalloc);
+			DISPLAY_STRUCT_HEX(dosHeader, e_ss);
+			DISPLAY_STRUCT_HEX(dosHeader, e_sp);
+			DISPLAY_STRUCT_HEX(dosHeader, e_csum);
+			DISPLAY_STRUCT_HEX(dosHeader, e_ip);
+			DISPLAY_STRUCT_HEX(dosHeader, e_cs);
+			DISPLAY_STRUCT_HEX(dosHeader, e_lfarlc);
+			DISPLAY_STRUCT_HEX(dosHeader, e_ovno);
+			DISPLAY_STRUCT_HEX(dosHeader, e_res); // 输出数组指针
+			DISPLAY_STRUCT_HEX(dosHeader, e_oemid);
+			DISPLAY_STRUCT_HEX(dosHeader, e_oeminfo);
+			DISPLAY_STRUCT_HEX(dosHeader, e_res2); // 输出数组指针
+			DISPLAY_STRUCT_HEX(dosHeader, e_lfanew);
+		TO_LEFT
+		cout << endl;
+		if (pe.HasNtHeader32())
+		{
+			auto ntHeader = pe.NtHeader32();
+			//cout << "[Is 32 PE]" << endl;
+			// ntHeader
+			DISPLAY_HEX("[NtHeader 32]",ntHeader);
+			TO_RIGHT
+				DISPLAY_STRUCT_HEX(ntHeader, Signature); 			
+				DISPLAY_STRUCT_POINTER_HEX(ntHeader, FileHeader);
+				// FileHeader
+				TO_RIGHT
+					auto fileHeader = ntHeader->FileHeader;
+					DISPLAY_STRUCT_HEX2(fileHeader, Machine);
+					DISPLAY_STRUCT_HEX2(fileHeader, NumberOfSections);
+					DISPLAY_STRUCT_HEX2(fileHeader, TimeDateStamp);
+					DISPLAY_STRUCT_HEX2(fileHeader, PointerToSymbolTable);
+					DISPLAY_STRUCT_HEX2(fileHeader, NumberOfSymbols);
+					DISPLAY_STRUCT_HEX2(fileHeader, SizeOfOptionalHeader);
+					DISPLAY_STRUCT_HEX2(fileHeader, Characteristics);
+				TO_LEFT
+
+				// OptionalHeader
+				DISPLAY_STRUCT_POINTER_HEX(ntHeader, OptionalHeader);
+				auto optionalHeader = ntHeader->OptionalHeader;	
+				TO_RIGHT							
+					DISPLAY_STRUCT_HEX2(optionalHeader, Magic);
+					DISPLAY_STRUCT_HEX2(optionalHeader, MajorLinkerVersion);
+					DISPLAY_STRUCT_HEX2(optionalHeader, MinorLinkerVersion);
+					DISPLAY_STRUCT_HEX2(optionalHeader, SizeOfCode);
+					DISPLAY_STRUCT_HEX2(optionalHeader, SizeOfInitializedData);
+					DISPLAY_STRUCT_HEX2(optionalHeader, SizeOfUninitializedData);
+					DISPLAY_STRUCT_HEX2(optionalHeader, AddressOfEntryPoint);
+					DISPLAY_STRUCT_HEX2(optionalHeader, BaseOfCode);
+					DISPLAY_STRUCT_HEX2(optionalHeader, BaseOfData);
+					DISPLAY_STRUCT_HEX2(optionalHeader, ImageBase);
+					DISPLAY_STRUCT_HEX2(optionalHeader, SectionAlignment);
+					DISPLAY_STRUCT_HEX2(optionalHeader, FileAlignment);
+					DISPLAY_STRUCT_HEX2(optionalHeader, MajorOperatingSystemVersion);
+					DISPLAY_STRUCT_HEX2(optionalHeader, MinorOperatingSystemVersion);
+					DISPLAY_STRUCT_HEX2(optionalHeader, MajorImageVersion);
+					DISPLAY_STRUCT_HEX2(optionalHeader, MinorImageVersion);
+					DISPLAY_STRUCT_HEX2(optionalHeader, MajorSubsystemVersion);
+					DISPLAY_STRUCT_HEX2(optionalHeader, MinorSubsystemVersion);
+					DISPLAY_STRUCT_HEX2(optionalHeader, Win32VersionValue);
+					DISPLAY_STRUCT_HEX2(optionalHeader, SizeOfImage);
+					DISPLAY_STRUCT_HEX2(optionalHeader, SizeOfHeaders);
+					DISPLAY_STRUCT_HEX2(optionalHeader, CheckSum);
+					DISPLAY_STRUCT_HEX2(optionalHeader, Subsystem);
+					DISPLAY_STRUCT_HEX2(optionalHeader, DllCharacteristics);
+					DISPLAY_STRUCT_HEX2(optionalHeader, SizeOfStackReserve);
+					DISPLAY_STRUCT_HEX2(optionalHeader, SizeOfStackCommit);
+					DISPLAY_STRUCT_HEX2(optionalHeader, SizeOfHeapReserve);
+					DISPLAY_STRUCT_HEX2(optionalHeader, SizeOfHeapCommit);
+					DISPLAY_STRUCT_HEX2(optionalHeader, LoaderFlags);
+					DISPLAY_STRUCT_HEX2(optionalHeader, NumberOfRvaAndSizes);
+					DISPLAY_STRUCT_HEX2(optionalHeader, DataDirectory);
+
+					// 数据目录
+					TO_RIGHT						
+						auto dataDirectory = optionalHeader.DataDirectory;
+						for (int i = 0; i < optionalHeader.NumberOfRvaAndSizes; i++)
+						{
+							auto dataDir = dataDirectory++;
+	#define DISPLAY_DATA_DIR(name) DISPLAY_HEX(#name, dataDir);\
+			TO_RIGHT\
+			DISPLAY_STRUCT_HEX(dataDir,VirtualAddress);\
+			DISPLAY_STRUCT_HEX(dataDir,Size);\
+			TO_LEFT							
+							switch (i)
+							{
+							case IMAGE_DIRECTORY_ENTRY_EXPORT:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_EXPORT);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_IMPORT:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_IMPORT);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_RESOURCE:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_RESOURCE);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_EXCEPTION:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_EXCEPTION);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_SECURITY:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_SECURITY);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_BASERELOC:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_BASERELOC);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_DEBUG:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_DEBUG);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_ARCHITECTURE:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_ARCHITECTURE);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_GLOBALPTR:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_GLOBALPTR);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_TLS:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_TLS);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_IAT:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_IAT);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT);
+								break;
+							case IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR:
+								DISPLAY_DATA_DIR(IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR);
+								break;
+							default:
+								break;
+							}						
+						}
+						TO_LEFT
+					// end dataDirectory
+					TO_LEFT
+				// end optionalHeader
+				TO_LEFT
+				// end ntheader
+				cout << endl;
+				DISPLAY_MSG("[Section]");
+				PE::SectionReader sr(pe);
+				while (sr.Next())
+				{
+					auto tmpSection = sr.Current();
+					DISPLAY_HEX(tmpSection->Name, tmpSection);
+					TO_RIGHT						
+						DISPLAY_STRUCT_HEX(tmpSection, Misc.PhysicalAddress);
+						DISPLAY_STRUCT_HEX(tmpSection, Misc.VirtualSize);
+						DISPLAY_STRUCT_HEX(tmpSection, VirtualAddress); // 节位置rva
+						DISPLAY_STRUCT_HEX(tmpSection, SizeOfRawData);
+						DISPLAY_STRUCT_HEX(tmpSection, PointerToRawData); // 文件位置偏移量
+						DISPLAY_STRUCT_HEX(tmpSection, PointerToRelocations);
+						DISPLAY_STRUCT_HEX(tmpSection, PointerToLinenumbers);
+						DISPLAY_STRUCT_HEX(tmpSection, NumberOfRelocations);
+						DISPLAY_STRUCT_HEX(tmpSection, NumberOfLinenumbers);
+						DISPLAY_STRUCT_HEX(tmpSection, Characteristics);
+					TO_LEFT
+				}
+				cout << endl;
+				DISPLAY_MSG("[Export]");
+				TO_RIGHT
+					auto export = pe.GetImageExport();
+					DISPLAY_STRUCT_HEX(export, Characteristics);
+					DISPLAY_STRUCT_HEX(export, TimeDateStamp);
+					DISPLAY_STRUCT_HEX(export, MajorVersion);
+					DISPLAY_STRUCT_HEX(export, MinorVersion);
+					//DISPLAY_STRUCT_HEX(export, Name);
+					
+					DISPLAY_HEX("Name", export->Name<< " ["<<(PCHAR)pe.GetRvaData(export->Name)<<"]");
+					DISPLAY_STRUCT_HEX(export, Base);
+					DISPLAY_STRUCT_HEX(export, NumberOfFunctions);
+					DISPLAY_STRUCT_HEX(export, NumberOfNames);
+					DISPLAY_STRUCT_HEX(export, AddressOfFunctions);
+					DISPLAY_STRUCT_HEX(export, AddressOfNames);
+					DISPLAY_STRUCT_HEX(export, AddressOfNameOrdinals);
+					TO_RIGHT
+						PE::ExportReader er(pe);
+						while (er.Next())
+						{
+							DISPLAY_HEX("Ordinals", *er.CurrentNameOrdinals());
+							TO_RIGHT
+							auto nameRva = er.CurrentNameRva();
+							if (nameRva)
+							{
+								auto name = (PCHAR)pe.GetRvaData(*nameRva);
+								DISPLAY_MSG("Name ["<<name<<"]");
+
+								// 附加测试 - 运行读取到的导出函数	
+								if (strcmp(name, "B") == 0 || strcmp(name, "?A@@YAPADXZ") == 0)
+								{
+									auto function = (PCHAR(*)())pe.GetRvaData(*er.CurrentFuncRva());
+									DISPLAY_MSG("=== Call Function === ["<<function()<<"]");
+								}
+								
+								// 附加测试结束
+
+								DISPLAY_HEX("NameRva", *nameRva);
+								
+							}
+					
+							DISPLAY_HEX("FuncRva", *er.CurrentFuncRva());
+							TO_LEFT
+						}
+					TO_LEFT
+					// TODO: 导入表
+
+				TO_LEFT
+		}
+		else
+		{
+			auto ntHeader = pe.NtHeader64();
+			DISPLAY_HEX("NtHeader64",ntHeader);
+			DISPLAY_STRUCT_HEX(ntHeader, Signature);
+			DISPLAY_STRUCT_POINTER_HEX(ntHeader, FileHeader);
+			DISPLAY_STRUCT_POINTER_HEX(ntHeader, OptionalHeader);
+		}
+
+
+
+
+	}
+	else
+	{
+		cout << "Not a PE" << endl;
+	}
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+
 	//_asm pushad
 	//InitLazyLoad();
 	////InitLazyLoad();
 	//Lazy_MessageBoxA(0, 0, 0, 0);
 	//MessageBoxA(0, 0, 0, 0);
-	auto x = Peb::FindLoadedModuleHandle(L"ntdll.dll");
+	//auto x = Peb::FindLoadedModuleHandle(L"ntdll.dll");
+
+	TestPeDecoder();
+
+
+
 	return 0;
 }
+#pragma region 导出表测试数据
+#define Export __declspec(dllexport)
 
+Export PCHAR  A()
+{
+	return "A - Success";
+}
+extern "C"
+Export PCHAR B()
+{
+	return "B - Success";
+}
+class MyClass
+{
+public:
+	Export MyClass(){}
+	Export ~MyClass(){}
+
+private:
+	Export void C(){}
+};
+#pragma endregion
